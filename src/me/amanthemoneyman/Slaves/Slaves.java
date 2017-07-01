b4761549d84e7089083281e4aa4e58b1f473c3fb
BuildTools <unconfigured@null.spigotmc.org>
2017-06-30 20:17:37 -0700

    Test

---
+++ src/me/amanthemoneyman/Slaves/Slaves.java
@@ -0,0 +1,25 @@
+package me.amanthemoneyman.Slaves;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by alexm on 6/30/2017.
+ */
+public class Slaves extends JavaPlugin {
+
+    public void onEnable()
+    {
+
+
+
+
+    }
+
+    public void onDisable()
+    {
+
+
+
+
+    }
+}
